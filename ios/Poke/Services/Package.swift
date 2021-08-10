0fcf88f2f98633696df6b387e51a906588e95f62
Dylan Velez <dylan.velez@gorillalogic.com>
2021-08-10 14:01:08 -0500

    Removed Test groups on packages

--- ios/Poke/Services/Package.swift
+++ ios/Poke/Services/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Services",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,9 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Services",
-            dependencies: []),
-        .testTarget(
-            name: "ServicesTests",
-            dependencies: ["Services"]),
+            dependencies: [])
     ]
 )
